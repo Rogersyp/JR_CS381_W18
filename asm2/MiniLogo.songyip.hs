fdcec58a2567fbedf9a601fae5994cf164eafe85
fischjer4 <fischjer4@gmail.com>
2018-01-30 13:44:27 -0800

    jeremy added his number 4 to comments

--- asm2/MiniLogo.songyip.hs
+++ asm2/MiniLogo.songyip.hs
@@ -34,7 +34,6 @@ data Cmd   = Pen Mode                -- change pen mode
 --          pen up; move (x1, y1)
 --          pen down; move (x2, y2)
 --      }    	
-
 line :: Cmd
 line = Define "line" ["x1", "y1", "x2", "y2"]
        [Pen Up, Move (Ref "x1", Ref "y1"), Pen Down, Move (Ref "x2", Ref "y2")]
@@ -47,10 +46,35 @@ line = Define "line" ["x1", "y1", "x2", "y2"]
 --          line(x1, y1, x1 + w, y1 + h) -- draw /
 --          line(x1, y1 + h, x1 + w, y1) -- draw \
 --      }    
-
 nix::Cmd
 nix = Define "nix" ["x1", "y1", "w", "h"]
        [
-        Call "line" [Ref "x1", Ref "y1", Add Ref "x1" Ref "w", Add Ref "y1" Ref "h"], 
-        Call "line" [Ref "x1", Add Ref "y1" Ref "h", Add Ref "x1" Ref "w", Ref "y1"] 
+        Call "line" [Ref "x1", Ref "y1", Add (Ref "x1") (Ref "w"), Add (Ref "y1") (Ref "h")], 
+        Call "line" [Ref "x1", Add (Ref "y1") (Ref "h"), Add (Ref "x1") (Ref "w"), Ref "y1"] 
        ]
+
+
+-- 4. 
+--      constructs a MiniLogo program that draws a staircase of n 
+--      steps starting from (0,0).
+--      define stair(xStart, yStart){
+--          line(xStart, yStart, xStart, yStart + 1)     -- draw |
+--          line(xStart, yStart + 1, xStart + 1, yStart) -- draw --
+--      }
+--
+--
+-- stair::Cmd
+-- stair = Define "stair" ["xStart", "yStart"]
+--        [
+--         Call "line" [Ref "xStart", Ref "yStart", Ref "xStart", Add (Ref "yStart") (Ref "1")], 
+--         Call "line" [Ref "xStart", Add (Ref "yStart") (Ref "1"), Add (Ref "xStart") (Ref "1"), Ref "yStart"] 
+--        ]
+-- -- How it works:
+-- --      - draw a stair going up then right starting from (n-1)(n-1)
+-- --      - recursivly draw a single stair until 0 is reached
+-- --      - when 0 is reached, stop, lift pen up
+-- steps :: Int -> Prog
+-- steps n = if(n <= 0) 
+--             then [Pen Up] 
+--           else
+--             [Call "stair" [Lit (pred n), Lit (pred n)]] ++ steps(pred n)
